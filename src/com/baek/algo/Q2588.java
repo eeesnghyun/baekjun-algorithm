0d4fa613d4453f5a0cf3d46a6e46215eb1de6cdc
Seunghyun Lee <szhyun2002@gmail.com>
2021-07-05 10:38:08 +0900

    1st commit

---
+++ src/com/baek/algo/Q2588.java
@@ -0,0 +1,33 @@
+package com.baek.algo;
+
+import java.util.Scanner;
+
+public class Q2588 {
+	public static void main(String[] args) {
+		/*
+		 * (세 자리 수) × (세 자리 수)는 다음과 같은 과정을 통하여 이루어진다.
+		 * (1)과 (2)위치에 들어갈 세 자리 자연수가 주어질 때 (3), (4), (5), (6)위치에 들어갈 값을 구하는 프로그램을 작성하시오.
+		 *    472 - (1)
+		 * x  385 - (2)
+		 * ------
+		 *   2360 - (3)
+		 *  3776  - (4)
+		 * 1416   - (5)
+		 * ------
+		 * 181720
+		 */
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int d   = a * (b % 10);
+		int e   = a * ((b / 10) % 10);
+		int f   = a * (b / 100);
+		int tot = a * b;
+
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(tot);
+	}
+}
---
+++ src/com/baek/algo/Q2884.java
@@ -0,0 +1,38 @@
+package com.baek.algo;
+
+import java.util.Scanner;
+
+public class Q2884 {
+
+	public static void main(String[] args) {
+		/*
+		 * 45분 일찍 설정되는 알람 프로그램
+		 *
+		 * 입력 : 두 정수(H, M)
+		 * 조건
+		 * 1. 첫째 줄에 두 정수 H와 M이 주어진다. (0 ≤ H ≤ 23, 0 ≤ M ≤ 59) 그리고 이것은 현재 상근이가 설정한 놓은 알람 시간 H시 M분을 의미한다.
+		 * 2. 입력 시간은 24시간 표현을 사용한다. 24시간 표현에서 하루의 시작은 0:0(자정)이고, 끝은 23:59(다음날 자정 1분 전)이다. 시간을 나타낼 때, 불필요한 0은 사용하지 않는다.
+		 */
+		Scanner sc = new Scanner(System.in);
+
+		int hh = sc.nextInt();
+		int mm = sc.nextInt();
+
+		int rh = 0;
+		int rm = 0;
+
+		if(mm < 45) {
+			if(hh == 0) {
+				rh = 23;
+			} else {
+				rh = hh - 1;
+			}
+			rm = (mm + 60) - 45;
+		} else {
+			rh = hh;
+			rm = mm - 45;
+		}
+
+		System.out.println(rh + " " + rm);
+	}
+}
