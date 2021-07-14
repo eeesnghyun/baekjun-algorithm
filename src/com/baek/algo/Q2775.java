303e73113427b986eaf3b02f6a6c3036a8056f6e
eeesnghyun <szhyun2002@gmail.com>
2021-07-14 10:09:52 +0900

    add 2775

--- src/com/baek/algo/Q2775.java
+++ src/com/baek/algo/Q2775.java
@@ -13,36 +13,39 @@ public class Q2775 {
 		 * - 각각의 케이스마다 첫 번째 줄에 정수 K, 두 번째 줄에 정수 N이 주어진다.
 		 *
 		 * 조건
-		 * - 1 ≤ k, n ≤ 14
-		 * - k층 n호에 살려면 자신의 아래(k-1)층의 1호부터 n호까지 사람들의 수의 합만큼 사람들이 살아야한다.
+		 * - 1 ≤ K, N ≤ 14
+		 * - K층 N호에 살려면 자신의 아래(K-1)층의 1호부터 N호까지 사람들의 수의 합만큼 사람들이 살아야한다.
 		 * - 아파트는 0층부터 있고 각층은 1호부터 있으며 0층의 i호에는 i명이 산다.
 		 */
 		Scanner sc = new Scanner(System.in);
 
 		int t = sc.nextInt();
-		int k = sc.nextInt();
-		int n = sc.nextInt();
 
-		int cnt = 0;
-		int x = n;
-		int y = k + (n-1);
+		for(int i = 0; i < t; i++) {
+			int k = sc.nextInt();
+			int n = sc.nextInt();
 
-		if((k - 1) != 0) {
-			for(int i = 0; i < k - 1; i++) {
-				System.out.println("i : " + i);
-				System.out.println(x);
-				x = x + (n + i);
+			if(n >= 14) n = 14;
+
+			int[][] arr = new int[k + 1][n];
+
+			for(int x = 0; x <= k; x++) {
+				for(int y = 0; y < n; y++) {
+					int sum = 0;
+
+					if(x == 0) {
+						arr[x][y] = y + 1;
+					} else {
+						for(int j = 0; j <= y; j++) {
+							sum = sum + arr[x-1][j];
+						}
+						arr[x][y] = sum;
+					}
+				}
 			}
+
+			System.out.println(arr[k][n-1]);
 		}
-
-		cnt = x + y;
-
-		System.out.println("k : " + k);
-		System.out.println("n : " + n);
-		System.out.println("x : " + x);
-		System.out.println("y : " + y);
-
-		System.out.println(cnt);
 
 		sc.close();
 	}
